e31ad217df4610aeae302a8359663f998770c639
Kakaoss <Kubek007.w@wp.pl>
2022-03-23 08:29:18 +0100

    Zadanie skonczone

--- app/src/main/java/com/example/zadanienaocene_23032022/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene_23032022/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.zadanienaocene_23032022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +14,10 @@ class MainActivity : AppCompatActivity() {
             val odblokujdane = findViewById<Button>(R.id.btnOdb)
             val PlecM = findViewById<RadioButton>(R.id.Pm)
             val PlecK = findViewById<RadioButton>(R.id.Pk)
-            val Muzyka = findViewById<RadioButton>(R.id.radioMuzyka)
-            val Sport = findViewById<RadioButton>(R.id.radioSport)
-            val turystyka = findViewById<RadioButton>(R.id.radioTurystyka)
-            val gry = findViewById<RadioButton>(R.id.radioGry)
+            val Muzyka = findViewById<CheckBox>(R.id.checkMuzyka)
+            val Sport = findViewById<CheckBox>(R.id.checkSport)
+            val turystyka = findViewById<CheckBox>(R.id.checkTurystyka)
+            val gry = findViewById<CheckBox>(R.id.checkGry)
             val wiek = findViewById<EditText>(R.id.TextWiek)
 
 
@@ -40,6 +38,32 @@ class MainActivity : AppCompatActivity() {
             turystyka.isEnabled = true
             gry.isEnabled = true
             wiek.isEnabled = true
+        }
+
+        findViewById<Button>(R.id.btnWyswietl).setOnClickListener {
+            var plec = " "
+            var zainteresowania = " "
+            
+            if (PlecM.isChecked){
+                plec += "Mężczyzna"
+            }
+            if (PlecK.isChecked){
+                plec += "Kobieta"
+            }
+            if (Sport.isChecked){
+                zainteresowania += "Sport "
+            }
+            if (Muzyka.isChecked){
+                zainteresowania += "Muzyka "
+            }
+            if (turystyka.isChecked){
+                zainteresowania += "turystyka "
+            }
+            if (gry.isChecked){
+                zainteresowania += "gry "
+            }
+            var wynik = "\n Twoja płeć to : " + plec + "\n Twoj wiek to : " + wiek.text.toString() + " lat " + "\n Twoje zainteresowania to : " + zainteresowania
+            Toast.makeText(applicationContext, "Twoje dane to : \n" + wynik, Toast.LENGTH_SHORT).show()
         }
     }
 }
