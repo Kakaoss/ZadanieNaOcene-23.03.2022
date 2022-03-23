bf475027125e539a3956b4e6ceafc198fd126409
Kakaoss <Kubek007.w@wp.pl>
2022-03-23 08:01:09 +0100

    zablokowywanie i odblokowywanie skonczone

--- app/src/main/java/com/example/zadanienaocene_23032022/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene_23032022/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.zadanienaocene_23032022
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +14,32 @@ class MainActivity : AppCompatActivity() {
 
             val zablokujdane = findViewById<Button>(R.id.btnZab)
             val odblokujdane = findViewById<Button>(R.id.btnOdb)
-            var PlecM = findViewById<RadioButton>(R.id.Pm)
+            val PlecM = findViewById<RadioButton>(R.id.Pm)
+            val PlecK = findViewById<RadioButton>(R.id.Pk)
+            val Muzyka = findViewById<RadioButton>(R.id.radioMuzyka)
+            val Sport = findViewById<RadioButton>(R.id.radioSport)
+            val turystyka = findViewById<RadioButton>(R.id.radioTurystyka)
+            val gry = findViewById<RadioButton>(R.id.radioGry)
+            val wiek = findViewById<EditText>(R.id.TextWiek)
 
 
         zablokujdane.setOnClickListener {
-
+            PlecM.isEnabled = false
+            PlecK.isEnabled = false
+            Muzyka.isEnabled = false
+            Sport.isEnabled = false
+            turystyka.isEnabled = false
+            gry.isEnabled = false
+            wiek.isEnabled = false
+        }
+        odblokujdane.setOnClickListener {
+            PlecM.isEnabled = true
+            PlecK.isEnabled = true
+            Muzyka.isEnabled = true
+            Sport.isEnabled = true
+            turystyka.isEnabled = true
+            gry.isEnabled = true
+            wiek.isEnabled = true
         }
     }
 }
