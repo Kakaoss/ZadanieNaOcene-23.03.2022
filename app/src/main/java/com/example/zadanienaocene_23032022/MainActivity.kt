862f8332ceb1641780165f8f8653961156165b13
Kakaoss <Kubek007.w@wp.pl>
2022-03-23 07:50:43 +0100

    activity wyglad skonczone

--- app/src/main/java/com/example/zadanienaocene_23032022/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene_23032022/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.zadanienaocene_23032022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+            val zablokujdane = findViewById<Button>(R.id.btnZab)
+            val odblokujdane = findViewById<Button>(R.id.btnOdb)
+            var PlecM = findViewById<RadioButton>(R.id.Pm)
+
+
+        zablokujdane.setOnClickListener {
+
+        }
     }
 }
